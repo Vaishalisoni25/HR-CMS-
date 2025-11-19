86715c8db66796b39a5523d7c56de7e13e360e50
Vaishalisoni25 <vaishalisoni02004@gmail.com>
2025-11-19 14:21:30 +0530

    optimized_code

--- server.js
+++ server.js
@@ -20,4 +20,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running  at  http://localhost::${PORT}`)
+);
