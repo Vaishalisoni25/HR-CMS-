cd76e79f5d604575ea66a065f935ddae7c78fa60
Vaishalisoni25 <vaishalisoni02004@gmail.com>
2025-12-15 16:06:22 +0530

    Added user register

--- middlewares/role.middleware.js
+++ middlewares/role.middleware.js
@@ -1,12 +1,12 @@
-export default function (...allowedRoles) {
-  return (req, res) => {
-    if (!req.user)
-      return res.status(401).json({ message: "Not authenticated" });
+// export default function (...allowedRoles) {
+//   return (req, res) => {
+//     if (!req.user)
+//       return res.status(401).json({ message: "Not authenticated" });
 
-    const userRole = req.user?.role;
+//     const userRole = req.user?.role;
 
-    if (!allowedRoles.includes(userRole)) {
-      return res.status(403).json({ message: "Access denied!" });
-    }
-  };
-}
+//     if (!allowedRoles.includes(userRole)) {
+//       return res.status(403).json({ message: "Access denied!" });
+//     }
+//   };
+// }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,9 @@ import express from "express";
 import { register, login } from "../controllers/user.controller.js";
 import validate from "../middlewares/validate.middleware.js";
 import { registerSchema, loginSchema } from "../validations/auth.validation.js";
+import { ROLES } from "../config/constant.js";
+import auth from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/register", validate(registerSchema), register);
