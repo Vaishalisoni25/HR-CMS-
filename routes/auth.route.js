3ab47a038f69cde97e0ba858c12bcefab3b5a230
Vaishalisoni25 <vaishalisoni02004@gmail.com>
2025-12-15 18:06:54 +0530

    Update Employee Validation issue fixed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { register, login } from "../controllers/user.controller.js";
+import {
+  register,
+  login,
+  getAllUsers,
+} from "../controllers/user.controller.js";
 import validate from "../middlewares/validate.middleware.js";
 import { registerSchema, loginSchema } from "../validations/auth.validation.js";
 import { ROLES } from "../config/constant.js";
@@ -9,4 +13,5 @@ const router = express.Router();
 
 router.post("/register", validate(registerSchema), register);
 router.post("/login", validate(loginSchema), login);
+router.get("/", ([ROLES.HR, ROLES.SUPERADMIN], getAllUsers));
 export default router;
